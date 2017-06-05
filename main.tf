5e41b988007f64d045c2d33d6002010208381232
Lionel Villard <villard@us.ibm.com>
2017-06-05 16:28:46 -0400

    Delete devops files. Instead use terraform

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "ibmcloud" {
+  bluemix_api_key = "${var.bluemix_api_key}"
+}
+
+data "ibmcloud_cf_space" "spacedata" {
+  space = "${var.cf_space}"
+  org   = "${var.cf_org}"
+}
+
+# Provision Cloudant
+resource "ibmcloud_cf_service_instance" "cloudant" {
+  name       = "${var.database}"
+  space_guid = "${data.ibmcloud_cf_space.spacedata.id}"
+  service    = "cloudantNoSQLDB"
+  plan       = "Lite"
+  tags       = ["cluster-service", "cluster-bind"]
+}
+
+# Create credentials
+resource "ibmcloud_cf_service_key" "cloudant" {
+  name                  = "cloudantkey"
+  service_instance_guid = "${ibmcloud_cf_service_instance.cloudant.id}"
+}
+
+//
+//output "credentials" {
+//  value = "${ibmcloud_cf_service_key.cloudant.credentials}"
+//}
+//
+//output "guid" {
+//  value = "${ibmcloud_cf_service_instance.cloudant.service_plan_guid}"
+//}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "bluemix_api_key" {
+  type = "string"
+}
+
+variable "cf_org" {
+  type = "string"
+}
+
+variable "cf_space" {
+  type = "string"
+}
+
+variable "database" {
+  type = "string"
+}
